9f3f3a13ddb14b2287bca93a06e6610794896485
Attila Szabo <attila.szabo.vrc@gmail.com>
2020-06-04 18:42:09 +0200

    heroku  files added, heroku port added. READY to deploy

--- app.js
+++ app.js
@@ -202,6 +202,13 @@ app.post("/saveHero", function (req, res) {
 });
 
 //Listen
-app.listen(3000, function () {
-    console.log("Server is running on port 3000.");
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
+
+app.listen(port, function () {
+    console.log("Server has started successfully.");
 })
